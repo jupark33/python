b51d9a3b19e90770655ab8ae704fc91ebfe2b250
jupark33 <jupark33@gmail.com>
2018-02-21 22:51:13 +0900

    내용 추가

--- kiwoom/db/alc_1.py
+++ kiwoom/db/alc_1.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import datetime
+import ConfigParser
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -11,3 +13,24 @@ class Stock_info(Base):
 	__tablename__ = 'T_stock_info'
 	no = Column(Integer, primary_key=True)
 	st_code = Column(String(20), nullable=False)
+	st_name = Column(String(50), nullable=False)
+
+class Volumes(Base):
+	__tablename__ = 'T_volumes'
+	no = Column(Integer, primary_key=True)
+	st_code = Column(String(20), nullable=False)
+	volume = Column(Integer, nullable=False)
+	dt_date = Column(Datetime)
+	dt_time = Column(Datetime)
+
+config = ConfigParser.RawConfigParser()
+config.read('db_conn.properties')
+
+id = config.get('connection', 'id')
+pw = config.get('connection', 'pw')
+
+#engine = create_engine('sqlite:///sqlalchemy_example.db')
+url = 'postgresql://' + id + ":" + pw + '@localhost:5432/mms'
+engine = create_engine(url)
+
+Base.metadata.create_all(engine)
