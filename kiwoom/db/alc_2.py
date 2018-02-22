fd5a712b13f0e081f3d7d88873b11a919fe3bf6a
jupark33 <jupark33@gmail.com>
2018-02-22 09:46:46 +0900

    테이블 select 구현 중

--- kiwoom/db/alc_2.py
+++ kiwoom/db/alc_2.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 
 from alc_1 import Base, Stock_info, Volumes, engine
 
@@ -7,4 +8,13 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
+# s = select([T_stock_info])
 
+# for item in session.query(Stock_info).all():
+#     print(item)
+
+result = session.query(Stock_info).all()
+print(result)
+
+# for info in session.query(Stock_info).all():
+#     print(result.no)
