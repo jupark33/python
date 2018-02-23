c8a3aa917a953d085a747829a82739ae9cfd93d8
jupark33 <jupark33@gmail.com>
2018-02-23 18:20:23 +0900

    날짜의 시간을 23시 59분으로 강제 변환

---
+++ krx/insertDb/krx2db.py
@@ -0,0 +1,85 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
+import urllib3
+from xml.dom import minidom
+from bs4 import BeautifulSoup
+
+from db_engine import Base, Stock_info, Volume, engine
+from StringUtil import strCommaToInt, change_hour_minute
+from datetime import datetime, timedelta
+
+def get_basic_inf():
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    result = session.query(Stock_info).all()
+    print(result)
+
+    for info in session.query(Stock_info).all():
+        print('no:' + str(info.no) + ', st_code:' + info.st_code + ', st_name:' + info.st_name)
+
+def get_volume_n_price(st_code):
+    url = 'http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=' + st_code
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+
+    soup = BeautifulSoup(r.data, "lxml")
+
+    # print('--dailystock--')  # 디버깅 용도
+    dailystocks = soup.findAll('dailystock')
+    # print(dailystocks) # 디버깅 용도
+
+    print('st_code : ' + st_code)
+    print('date   volume     start    endprice')
+    for ds in dailystocks:
+        # print(ds['day_date'], ds['day_volume'], ds['day_start'], ds['day_endprice'])
+        new_volume = Volume(st_code=st_code, dt_date=ds['day_date'], volume=ds['day_volume'])
+        print(new_volume.st_code + ', ' + new_volume.dt_date + ', ' + new_volume.volume)
+
+class Processor_Volume(object):
+
+    def __init__(self):
+        DBSession = sessionmaker(bind=engine)
+        self.session = DBSession()
+
+    def krx_2_db(self, st_code):
+        url = 'http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=' + st_code
+        http = urllib3.PoolManager()
+        r = http.request('GET', url)
+
+        soup = BeautifulSoup(r.data, "lxml")
+
+        # print('--dailystock--')  # 디버깅 용도
+        dailystocks = soup.findAll('dailystock')
+        # print(dailystocks) # 디버깅 용도
+
+        print('st_code : ' + st_code)
+        for ds in dailystocks:
+            # print(ds['day_date'], ds['day_volume'], ds['day_start'], ds['day_endprice'])
+            str_date = ds['day_date']
+            date = datetime.strptime(str_date, '%y/%m/%d')
+
+            date = change_hour_minute(date, 23, 59)
+            new_volume = Volume(st_code=st_code, dt_date=date, volume=strCommaToInt(ds['day_volume']))
+            print(new_volume.st_code + ', ' + str(new_volume.dt_date) + ', ' + str(new_volume.volume))
+            # self.session.add(new_volume)
+        # self.session.commit()
+
+
+            # date     volume     start    endprice
+            # 18/02/23 10,303,817 4,525    4,680
+            # 18/02/22 23,797,843 4,580    4,530
+            # 18/02/21 73,431,572 4,250 4,650
+            # 18/02/20 47,410,354 3,300 4,290
+            # 18/02/19 7,494,844 3,220 3,300
+            # 18/02/14 7,396,193 3,150 3,080
+            # 18/02/13 9,180,467 2,850 3,070
+            # 18/02/12 415,465 2,805 2,805
+            # 18/02/09 316,883 2,730 2,790
+            # 18/02/08 470,086 2,725 2,790
+
+if __name__ == '__main__':
+    # get_basic_inf()
+    # get_volume_n_price('004770')
+    p_volume = Processor_Volume()
+    p_volume.krx_2_db('004770')
